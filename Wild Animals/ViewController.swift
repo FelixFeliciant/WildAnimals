e7d0c2dee382400ff3b3358f002649e220804e5e
Felix Feliciant <felixfeliciant@Felixs-Mac.local>
2017-05-03 20:40:02 +0300

    first commit managed to create a single view cell to populate single images

--- Wild Animals/ViewController.swift
+++ Wild Animals/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib. haha
      
         readJson()
         
