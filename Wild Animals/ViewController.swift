b3ce9987205f3eaaf916b5ad8d470d7b664728e4
Felix Feliciant <felixfeliciant@Felixs-Mac.local>
2017-05-06 10:26:44 +0300

    managed to download all images without errors

--- Wild Animals/ViewController.swift
+++ Wild Animals/ViewController.swift
@@ -22,44 +22,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-     
+        
         readJson()
-        
-        fetchAnimals()
-        
-        
     }
     
-    func convertToDictionary(text: String) -> [String: Any]? {
-        if let data = text.data(using: .utf8) {
-            do {
-                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
-        return nil
-    }
-
+    
     
     private func readJson() {
         do {
             
             self.animals = [Animal]()
-            
-            
-            
             if let file = Bundle.main.url(forResource: "animals", withExtension: "json") {
                 let data = try Data(contentsOf: file)
                 let json = try JSONSerialization.jsonObject(with: data, options: [])
                 if let object = json as? [String: Any] {
-                    
                     //parse title:
-                    if let title = object["title"] as? String {
+                    if (object["title"] as? String) != nil {
                         
                     }
-                    
-                    
                     var isAnimal: Bool
                     
                     //parse the paragrapphs which is an array
@@ -76,7 +56,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                                 if images.count == 0 {
                                     isAnimal = false
                                 }
-                                
                                 
                                 var imageObjects = [[String:String]]()
                                 
@@ -105,9 +84,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                         }
                         
                     }
-               
+                    
                     // json is a dictionary
-//                    print(object)
+                    //                    print(object)
                 } else if let object = json as? [Any] {
                     // json is an array
                     print(object)
@@ -131,101 +110,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     
-    func fetchAnimals() {
-        
-        // json url : https://api.myjson.com/bins/yapvl
-        
-        let urlRequest = URLRequest(url: URL(string:"https://api.myjson.com/bins/yapvl")!)
-        
-        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            
-            if error != nil {
-                print(error)
-                
-            }
-            
-            self.animals = [Animal]()
-            
-            do {
-                // global json object containing titile(string), paragraps(array of objects)
-                let json  = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
-                
-                //parse title:
-                if let title = json["title"] as? String {
-                    
-                }
-                
-                
-                var isAnimal: Bool
-                
-                //parse the paragrapphs which is an array
-                if let paragraphs = json["paragraphs"] as? [[String: AnyObject]] {
-                    
-                    for paragraph in paragraphs {
-                        
-                        isAnimal = true
-                        
-                        let caption  = paragraph["caption"]
-                        let text  = paragraph["text"]
-                        if let images = paragraph["images"] as? [[String:AnyObject]] {
-                            
-                            if images.count == 0 {
-                                isAnimal = false
-                            }
-                            
-                            
-                            var imageObjects = [[String:String]]()
-                            
-                            for image in images {
-                                let imageName = image["name"] as? String
-                                let imageUrl = image["url"] as? String
-                                let imageExplanation = image["explanation"] as? String
-                                
-                                var imgDict = [String: String]()
-                                imgDict["name"] = imageName
-                                imgDict["url"] = imageUrl
-                                imgDict["explanation"] = imageExplanation
-                                imageObjects.append(imgDict)
-                                
-                            }
-                            
-                            if isAnimal == true {
-                                let animal = Animal()
-                                animal.caption = caption as! String?
-                                animal.text = text as! String?
-                                animal.images = imageObjects
-                                self.animals?.append(animal)
-                            }
-                        }
-                        
-                    }
-                    
-                }
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-                
-            } catch let error{
-                print(error)
-            }
-            
-        }
-        
-        task.resume()
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-
+        
         
         if !(self.animals?[indexPath.item].hasMultipleImages())! {
             let cell = tableView.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath) as! AnimalCell
@@ -244,7 +132,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             cell.imgView?.addGestureRecognizer(tapped)
             
             return cell
-
+            
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "animalCellTwo", for: indexPath) as! AnimaCellTwo
             
@@ -280,10 +168,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func TappedOnImage(sender:UITapGestureRecognizer){
         print("heelo")
         
-    
-        
     }
-
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -294,93 +179,95 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return self.animals?.count ?? 0
     }
     
-    
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
-
-
-
-
 
 extension UIImageView {
     
-    
-    func resizeImage(image: UIImage, newWidth: CGFloat) -> UIImage? {
-        
-        let scale = newWidth / image.size.width
-        let newHeight = image.size.height * scale
-        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
-        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
-        
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage
-    }
-    
-    
-    
-    func downloadImage(from url: String) {
+    func downloadImage(from imageUrl: String) {
         
         
         
-            let urlRequest = URLRequest(url: (URL(string: url.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!)!))
-            let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-                if error != nil {
-                    print(error)
-                    return
-                }
-                
-                DispatchQueue.main.async {
-//                    var temp: UIImage = UIImage(data: data!)!
-//                    self.image = self.resizeImage(image: temp, newWidth: 200)
-                    self.image = UIImage(data: data!)!
-                
+        //            let urlRequest = URLRequest(url: (URL(string: imageUrl.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!)!))
+        
+        var urlRequest:URLRequest
+        
+        let percentCharacter: Character = "%"
+        
+        if imageUrl.characters.contains(percentCharacter) {
+            urlRequest = URLRequest(url: URL(string: imageUrl)!)
+            print("before:\n \(imageUrl)\n")
+            print("urlRequestString is:\n \(urlRequest)\n")
+            print("************************************\n")
+        } else {
+            
+             urlRequest = URLRequest(url: (URL(string: imageUrl.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!)!))
+            print("before:\n \(imageUrl)\n")
+            print("urlRequestString is:\n \(urlRequest)\n")
+            print("************************************\n")
+        }
+        
 
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        //            imageUrl.addingPercentEncoding( withAllowedCharacters: .urlQueryAllowed)
+        //            urlRequest = urlRequest.replacingOccurrences(of: "%25", with: "", options: .literal, range: nil)
+        
+        
+        
+
+        
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            
+            //                print(urlRequest)
+            
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            let httpResponse = response as! HTTPURLResponse
+            
+            let statusCode = httpResponse.statusCode
+            
+            
+            if (statusCode == 200) {
+                DispatchQueue.main.async {
+                    self.image = UIImage(data: data!)!
+                    
+                    
                 }
             }
-            task.resume()
-        
             
-       
-        
-        
-
-        
-        
+            
+            
+            
+        }
+        task.resume()
         
     }
     
 }
 
 
-//
-//
-//
-//
-//
-//
-//                if let animalsFromJson = json["paragraphs"] as? [[String: AnyObject]] {
-//                    for animalFromJson in animalsFromJson {
-//
-//                        let animal = Animal()
-//
-//                        if let name = animalFromJson["caption"] as? String, let info = animalFromJson["text"] as? String, let imagesUrl = animalFromJson["images"] as? [AnyObject] {
-//
-//                            animal.caption = name
-//                            animal.text = info
-////                            animal.images = imagesUrl as! [String]
-//
-//
-//                        }
-//                        self.animals?.append(animal)
-//                    }
-//                }
+
+
+
+
+
+
